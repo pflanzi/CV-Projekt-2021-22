6e05cebb01e7b52ef3dc51f49b0385a06fc10d66
epixware <epixware@googlemail.com>
2021-12-02 17:02:46 +0100

    Added Shapes

--- main.py
+++ main.py
@@ -2,42 +2,109 @@ import cv2
 import numpy as np
 from scipy import ndimage
 from tqdm import tqdm
+from PIL import Image
+import glob
 
-img_rgb = cv2.imread('images/six_apples.jpg')
-img_template = cv2.imread('images/test_apple.jpg', 0)
-for i in tqdm(range(20, 100)):
-    template = img_template
-    scale_percent = i  # percent of original size
-    width = int(template.shape[1] * scale_percent / 100)
-    height = int(template.shape[0] * scale_percent / 100)
-    dim = (width, height)
 
-    # resize image
-    template = cv2.resize(template, dim, interpolation=cv2.INTER_AREA)
+class DetectionAlgorithm:
 
-    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+    def __init__(self):
+        self.width = 0
+        self.height = 0
+        self.img_rgb = cv2.imread('images/six_apples.jpg')
+        self.image_list = []
+        for picture in glob.glob('images/single_apples/*'):
+            image = cv2.imread(picture)
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            self.image_list.append(image)
 
-    temp_height, temp_width = template.shape
-    img_height, img_width = img_gray.shape
+    def detect(self, img_template):
+        for scale_percent in tqdm(range(20, 150, 10)):
+            template = img_template
+            self.width = int(template.shape[1] * scale_percent / 100)
+            self.height = int(template.shape[0] * scale_percent / 100)
+            dim = (self.width, self.height)
 
-    if temp_height >= img_height or temp_width >= img_width:
-        break
+            # resize image
+            template = cv2.resize(template, dim, interpolation=cv2.INTER_AREA)
+            # grayscale
+            img_gray = cv2.cvtColor(self.img_rgb, cv2.COLOR_BGR2GRAY)
 
-    for angle in range(0, 360, 10):
-        try:
-            w, h = template.shape[::-1]
-            res = cv2.matchTemplate(img_gray, ndimage.rotate(template, angle), cv2.TM_CCOEFF_NORMED)
-            threshold = 0.7
-            loc = np.where(res >= threshold)
+            temp_height, temp_width = template.shape
+            img_height, img_width = img_gray.shape
 
-            for pt in zip(*loc[::-1]):
-                cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 3)
-        except cv2.error as error:
-            continue
-            # Some rotation exceeds the height/width of the image but doesn't break the program
-            # random comment
+            # Template bigger then original Picture -> exit
+            if temp_height >= img_height or temp_width >= img_width:
+                break
 
-cv2.imshow("test", img_rgb)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-# cv2.imwrite('images/apples_found.jpg', img_rgb)
+            for angle in range(0, 360, 20):
+                try:
+                    # Template matching
+                    w, h = template.shape[::-1]
+                    res = cv2.matchTemplate(img_gray, ndimage.rotate(template, angle), cv2.TM_CCOEFF_NORMED)
+                    threshold_template = 0.9
+                    # TODO: Check for color in an area around Object + Template
+                    loc = np.where(res >= threshold_template)
+
+                    for pt in zip(*loc[::-1]):
+                        cv2.rectangle(self.img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 0), 1)
+
+                    # Shape matching
+                    '''
+                    ret, thresh = cv2.threshold(img_gray, 127, 255, 0)
+                    ret, thresh2 = cv2.threshold(img_template, 127, 255, 0)
+                    contour, hierarchy = cv2.findContours(thresh, 2, 1)
+                    cnt1 = contour[0]
+                    contour, hierarchy = cv2.findContours(thresh2, 2, 1)
+                    cnt2 = contour[0]
+                    ret = cv2.matchShapes(cnt1, cnt2, 1, 0.0)
+                    
+                    threshold_shape = 0.3
+                    loc = np.where(ret <= threshold_shape)
+                    '''
+
+                    _, threshold = cv2.threshold(img_gray, 127, 255, cv2.THRESH_BINARY)
+                    contours, _ = cv2.findContours(
+                        threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+                    i = 0
+                    for contour in contours:
+                        if i == 0:
+                            i = 1
+                            continue
+
+                        # cv2.approxPloyDP() function to approximate the shape
+                        approx = cv2.approxPolyDP(
+                            contour, 0.01 * cv2.arcLength(contour, True), True)
+
+                        # using drawContours() function
+                        cv2.drawContours(self.img_rgb, [contour], 0, (0, 0, 255), 5)
+
+                        # finding center point of shape
+                        m = cv2.moments(contour)
+                        if m['m00'] != 0.0:
+                            x = int(m['m10'] / m['m00'])
+                            y = int(m['m01'] / m['m00'])
+
+                        if len(approx) >= 20:
+                            cv2.putText(self.img_rgb, 'apple',
+                                        (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+
+                    for pt in zip(*loc[::-1]):
+                        cv2.rectangle(self.img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 0), 1)
+                except cv2.error as error:
+                    continue
+                    # Some rotation exceeds the height/width of the image but doesn't break the program
+
+    def main(self):
+        for image in self.image_list:
+            self.detect(image)
+        cv2.imshow("Apple Detection", self.img_rgb)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+
+program = DetectionAlgorithm()
+program.main()
+
+# cv2.imwrite('images/apples_found.jpg', self.img_rgb)
