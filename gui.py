cc8e8242ec52a7686570e0fcb2478a582ecde97a
franzi <franziska.niemeyer@fh-bielefeld.de>
2021-12-02 16:51:13 +0100

    added a placeholder image improved formatting in gui code

--- gui.py
+++ gui.py
@@ -1,13 +1,13 @@
 # ------ imports ------ #
 import traceback
-import PySimpleGUI as Psg
+import PySimpleGUI as sg
 import io
 from PIL import Image
 
 # for testing / configuration purposes => sg.main()
 
 # ------ theme ------ #
-Psg.theme('Light Green')
+sg.theme('Light Green')
 
 # ------ menu ------ #
 menu_def = [
@@ -18,27 +18,27 @@ menu_def = [
 
 # ------ options ------ #
 thresh = [
-    [Psg.Text('Threshold')],
-    [Psg.Slider(key='Threshold',
-                range=(0, 255),
-                default_value=0,
-                size=(20, 15),
-                orientation='horizontal')]
+    [sg.Text('Threshold')],
+    [sg.Slider(key='Threshold',
+               range=(0, 255),
+               default_value=0,
+               size=(20, 15),
+               orientation='horizontal')]
 ]
 
 options = [
-    [Psg.Column(thresh, pad=(10, 10))],
-    [Psg.Text(key='-EXPAND3-', pad=0), Psg.Button('Count', key='count_button', pad=(10, 15)),
-     Psg.Text(key='-EXPAND4-', pad=0)]
+    [sg.Column(thresh, pad=(10, 10))],
+    [sg.Text(key='-EXPAND3-', pad=0), sg.Button('Count', key='count_button', pad=(10, 15)),
+     sg.Text(key='-EXPAND4-', pad=0)]
 ]
 
 # ------ picture area ------ #
 pic = [
-    [Psg.Image(r'/home/franzi/CV Projekt 2021/pictures/test/Placeholder.svg', key='picture', pad=(5, (3, 10)))],
-    [Psg.Text(' ', key='path', font=('Ubuntu', 11, 'italic'), text_color='#919191')],
+    [sg.Image(r'images/test/Placeholder.svg', key='picture', pad=(5, (3, 10)))],
+    [sg.Text(' ', key='path', font=('Ubuntu', 11, 'italic'), text_color='#919191')],
     [
-        Psg.Text('Counted objects: ', pad=(5, 25), font=('Ubuntu', 14, 'bold italic')),
-        Psg.Text(' ', key='result', font=('Ubuntu', 14))
+        sg.Text('Counted objects: ', pad=(5, 25), font=('Ubuntu', 14, 'bold italic')),
+        sg.Text(' ', key='result', font=('Ubuntu', 14))
     ]
 ]
 
@@ -46,35 +46,35 @@ pic = [
 
 
 err_box = [
-    [Psg.Multiline(key='err',
-                   font=('Ubuntu', 10),
-                   autoscroll=True,
-                   size=(35, 25),
-                   auto_refresh=True,
-                   do_not_clear=True,
-                   disabled=True)]
+    [sg.Multiline(key='err',
+                  font=('Ubuntu', 10),
+                  autoscroll=True,
+                  size=(35, 25),
+                  auto_refresh=True,
+                  do_not_clear=True,
+                  disabled=True)]
 ]
 
 # ------ layout ------ #
 layout = [
-    [Psg.Menu(menu_def, font=('Ubuntu', 11), tearoff=True)],
-    [Psg.Text(key='-EXPAND1-', pad=0)],  # no functionality, simply used to center certain elements
+    [sg.Menu(menu_def, font=('Ubuntu', 11), tearoff=True)],
+    [sg.Text(key='-EXPAND1-', pad=0)],  # no functionality, simply used to center certain elements
     [
-        Psg.Frame(' Options ', options, vertical_alignment='t'),
-        Psg.Column(pic, justification='c', pad=(100, 3), vertical_alignment='t'),
-        Psg.Column(err_box, vertical_alignment='t')
+        sg.Frame(' Options ', options, vertical_alignment='t'),
+        sg.Column(pic, justification='c', pad=(100, 3), vertical_alignment='t'),
+        sg.Column(err_box, vertical_alignment='t')
     ],
-    [Psg.Text(key='-EXPAND2-', pad=0)]  # no functionality, simply used to center certain elements
+    [sg.Text(key='-EXPAND2-', pad=0)]  # no functionality, simply used to center certain elements
 
 ]
 
 # ------ main ------ #
-window = Psg.Window('Analyzing pictures and counting objects',
-                    layout,
-                    font=('Ubuntu', 12),
-                    resizable=True,
-                    finalize=True,
-                    location=(0, 0))
+window = sg.Window('Analyzing pictures and counting objects',
+                   layout,
+                   font=('Ubuntu', 12),
+                   resizable=True,
+                   finalize=True,
+                   location=(0, 0))
 
 # resizing the window to fullscreen without making the titlebar/toolbar disappear
 width = window.TKroot.winfo_screenwidth()
@@ -92,7 +92,7 @@ while True:
     print(event, values)
 
     if event == 'Open':
-        file_path = Psg.popup_get_file(' ', title='Please chose a file', no_window=True)
+        file_path = sg.popup_get_file(' ', title='Please chose a file', no_window=True)
 
         # opening the image and converting it into a byte stream to display it inside the main window
         try:
@@ -116,7 +116,7 @@ while True:
                 '************************************************************************************************************\n')
             pass
 
-    if event == Psg.WIN_CLOSED or event == 'Close':
+    if event == sg.WIN_CLOSED or event == 'Close':
         break
 
 window.close()
