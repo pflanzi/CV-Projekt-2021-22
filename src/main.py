1300a7698f97402ee3c97be7d2ce06fb38db1a4f
epixware <epixware@googlemail.com>
2021-12-08 15:52:34 +0100

    Added before NMS

--- src/main.py
+++ src/main.py
@@ -43,21 +43,23 @@ class DetectionAlgorithm:
                     # Template matching
                     # TODO: Check multi-template matching
                     # below an example code of how multi-template matching could work
-                    # result = cv2.matchTemplate(imageGray, templateGray, cv2.TM_CCOEFF_NORMED)
-                    #
-                    # # ----- get all the coordinates where the matching result is >= threshold
-                    # threshold = 0.7
-                    # (yCoords, xCoords) = np.where(result >= threshold)
-                    # clone = image.copy()
-                    #
-                    # print("[INFO] {} matched locations *before* NMS".format(len(yCoords)))
-                    #
-                    # # ----- loop over the x- and y-coordinates and draw the bounding boxes
-                    # for (x, y) in zip(xCoords, yCoords):
-                    #     cv2.rectangle(clone, (x, y), (x + tW, y + tH), (255, 0, 0), 3)
-                    #
-                    # cv2.imshow("Before NMS", clone)
+                    w, h = template.shape[::-1]
+                    result = cv2.matchTemplate(img_gray, img_template, cv2.TM_CCOEFF_NORMED)
 
+                    # ----- get all the coordinates where the matching result is >= threshold
+                    threshold = 0.7
+                    (yCoords, xCoords) = np.where(result >= threshold)
+                    clone = self.img_rgb.copy()
+
+                    # print("[INFO] {} matched locations *before* NMS".format(len(yCoords)))
+
+                    # ----- loop over the x- and y-coordinates and draw the bounding boxes
+                    for (x, y) in zip(xCoords, yCoords):
+                        cv2.rectangle(clone, (x, y), (x + w, y + h), (255, 0, 0), 3)
+
+                    cv2.imshow("Before NMS", clone)
+
+                    '''
                     w, h = template.shape[::-1]
                     result = cv2.matchTemplate(img_gray, ndimage.rotate(template, angle), cv2.TM_CCOEFF_NORMED)
                     match_thresh = 0.9
@@ -67,7 +69,7 @@ class DetectionAlgorithm:
 
                     for pt in zip(*loc[::-1]):
                         cv2.rectangle(self.img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 0), 1)
-
+                    '''
                 except cv2.error as error:
                     continue
                     # Some rotation exceeds the height/width of the image but doesn't break the program
