97affcd3ebbd752e5ecd65b1a92a2e7cb69f1477
franzi <franziska.niemeyer@fh-bielefeld.de>
2021-12-09 14:39:31 +0100

    added TODO removed template code adjusted constructor

--- src/main.py
+++ src/main.py
@@ -9,7 +9,8 @@ import glob
 # TODO: add documentation
 # TODO: check for obsolete code pieces
 # TODO: check formatting
-# TODO: adjust Hough Circle detection
+# TODO: adjust Hough Circle detection (overlapping apples)
+# TODO: get more test images
 # TODO: add another processing step to filter / improve results
 # TODO: connect this code to the GUI
 
@@ -29,76 +30,55 @@ class DetectionAlgorithm:
                 template image height
             img_rgb : numpy.ndarray
                 stores a colored image (channels are in BGR order)
-            image_list : list
-                stores template images
         """
+        self.img_rgb = cv2.imread('../images/multiple_apples.jpg')
 
-        self.width = 0
-        self.height = 0
-        self.img_rgb = cv2.imread('images/multiple_apples.jpg')
-        self.image_list = []
-        for picture in glob.glob('images/single_apples/*'):
-            image = cv2.imread(picture)
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            self.image_list.append(image)
+        if self.img_rgb is not None:
+            self.width = self.img_rgb.shape[1]
+            self.height = self.img_rgb.shape[0]
+        else:
+            print(f"[INFO] Could not load image")
 
-    def detect(self, img_template):
+    def detect(self):
         """
         Function that performs the actual detection of circles inside a given image.
-        :param img_template: ???
         """
-        for scale_percent in tqdm(range(20, 150, 10)):
-            self.width = int(img_template.shape[1] * scale_percent / 100)
-            self.height = int(img_template.shape[0] * scale_percent / 100)
-            dim = (self.width, self.height)
+        # grayscale
+        img_gray = cv2.cvtColor(self.img_rgb, cv2.COLOR_BGR2GRAY)
 
-            # resize image
-            template = cv2.resize(img_template, dim, interpolation=cv2.INTER_AREA)
+        self.height, self.width = img_gray.shape
 
-            # grayscale
-            img_gray = cv2.cvtColor(self.img_rgb, cv2.COLOR_BGR2GRAY)
+        output = self.img_rgb.copy()
 
-            th, tw = template.shape
-            ih, iw = img_gray.shape
+        # detect circles in the image
+        circles = cv2.HoughCircles(img_gray, cv2.HOUGH_GRADIENT, 1,
+                                   minDist=58,
+                                   param1=90,
+                                   param2=45,
+                                   minRadius=65,
+                                   maxRadius=105)
 
-            # Template bigger than original Picture -> exit
-            if th >= ih or tw >= iw:
-                break
+        # ensure at least some circles were found
+        if circles is not None:
+            # convert the (x, y) coordinates and radius of the circles to integers
+            circles = np.round(circles[0, :]).astype("int")
 
-            output = self.img_rgb.copy()
+            # loop over the (x, y) coordinates and radius of the circles
+            for (x, y, r) in circles:
+                # draw the circle in the output image, then draw a rectangle
+                # corresponding to the center of the circle
+                cv2.circle(output, (x, y), r, (0, 255, 0), 4)
+                cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
 
-            # detect circles in the image
-            circles = cv2.HoughCircles(img_gray, cv2.HOUGH_GRADIENT, 1, 58,
-                                       param1=90,
-                                       param2=45,
-                                       minRadius=65,
-                                       maxRadius=105)
-
-            # ensure at least some circles were found
-            if circles is not None:
-                # convert the (x, y) coordinates and radius of the circles to integers
-                circles = np.round(circles[0, :]).astype("int")
-
-                # loop over the (x, y) coordinates and radius of the circles
-                for (x, y, r) in circles:
-                    # draw the circle in the output image, then draw a rectangle
-                    # corresponding to the center of the circle
-                    cv2.circle(output, (x, y), r, (0, 255, 0), 4)
-                    cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
-
-                cv2.imshow("Test", output)
-                # cv2.imshow("Gray", img_gray)
-                break
+            cv2.imshow("Test", output)
+            # cv2.imshow("Gray", img_gray)
 
     def main(self):
         """
         TODO: add some description here
         :return:
         """
-        # for image in self.image_list:
-        #     self.detect(image)
-        self.detect(self.image_list[0])
-        # cv2.imshow("Apple Detection", self.img_rgb)
+        self.detect()
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
@@ -106,6 +86,5 @@ class DetectionAlgorithm:
 # ----- main program ----- #
 program = DetectionAlgorithm()
 program.main()
-
 
 # cv2.imwrite('images/apples_found.jpg', self.img_rgb)
