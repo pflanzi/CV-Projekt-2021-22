f8aec0aafbc717537757befe3a91868ba4649c3d
franzi <franziska.niemeyer@fh-bielefeld.de>
2021-12-09 17:50:17 +0100

    readded docstrings and to-do-list

--- src/main.py
+++ src/main.py
@@ -1,10 +1,35 @@
+# ----- import ----- #
 import cv2
 import numpy as np
 
 
+# ----- To-Do-List ----- #
+# TODO: add documentation
+# TODO: check for obsolete code pieces
+# TODO: check formatting
+# TODO: adjust Hough Circle detection (overlapping apples)
+# TODO: get more test images
+# TODO: add another processing step to filter / improve results
+# TODO: connect this code to the GUI
+
+# ----- class, functions, variables ----- #
 class DetectionAlgorithm:
+    """
+    Detection Algorithm class containing functions
+    for detecting and counting objects in a given image
+    """
 
     def __init__(self):
+        """
+        Class constructor initializing the following class attributes:
+            width : int
+                template image width
+            height : int
+                template image height
+            img_rgb : numpy.ndarray
+                stores a colored image (channels are in BGR order)
+        """
+
         # self.img_rgb = cv2.imread('images/six_apples.jpg')
         # self.img_rgb = cv2.imread('images/3_apples.jpg')
         # self.img_rgb = cv2.imread('images/multiple_apples.jpg')
@@ -12,6 +37,9 @@ class DetectionAlgorithm:
         self.hsv = cv2.cvtColor(self.img_bgr, cv2.COLOR_BGR2HSV)
 
     def detect(self):
+        """
+        Function that performs the actual detection of circles inside a given image.
+        """
 
         output = self.img_bgr.copy()
         # detect circles in the image
@@ -35,6 +63,11 @@ class DetectionAlgorithm:
             cv2.imshow("Test", output)
 
     def main(self):
+        """
+        TODO: add some description here
+        :return:
+        """
+
         self.detect()
         cv2.waitKey(0)
         cv2.destroyAllWindows()
